4a119e99bb284df2e5c36a0a94f0997a5aeed02c
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-07 16:31:50 +0330

    Add Header Instruction

--- src/components/Header/toolbar-navigation.vue
+++ src/components/Header/toolbar-navigation.vue
@@ -1,254 +1,54 @@
 <template>
-  <q-toolbar class="text-primary shadow-6 column justify-start items-center q-py-md">
-    <div class="row justify-between items-center">
-      <q-btn @click="toggleSidebar" flat class="text-white hambergur-menu" icon="menu"></q-btn>
-      <div class="login-btn-group justify-between items-center ">
-        <q-btn class="register-btn"><a>ثبت نام</a></q-btn>
-        <q-btn class="entrance-btn"><a>ورود</a></q-btn>
+  <q-toolbar >
+    <div class="row justify-between items-center toolbar-content">
+      <div class="row justify-around items-center q-gutter-sm ">
+        <q-btn class="register-btn"><a class="q-mx-md">ثبت نام</a><q-img src="../../assets/RegisterIcon.png" width="24px"></q-img></q-btn>
+        <q-btn class="entrance-btn"><a class="q-mx-md">ورود</a><q-img src="../../assets/LoginIcon.png" width="24px"></q-img></q-btn>
+        <q-btn class="text-black q-px-md search-btn" icon="search"></q-btn>
       </div>
-      <q-toolbar-title  class="text-white title-container  q-ml-auto">
-        <h6 class="title">{{ InsuranceHeaderInfo?.centreName }}</h6>
-      </q-toolbar-title>
-    </div>
-    <div class="row justify-start items-center q-ml-auto">
-      <q-list v-if="!sidebarVisible" class="navLink-container row justify-start items-center">
-          <q-item v-for="(item, index) in sortedMenuItems"  :key="index"   class="navLink-item">
-            <submenu class="navLink-btn" :isSubMenu="hasChildItems(item.id)" :menus="getChildItems(item.id)" :menu="item" :allMenu = "InsuranceNavbarMenuItems" isFirst/>
-          </q-item>
-          
-  
-      </q-list>
-      <q-drawer v-else v-model="sidebarVisible" side="left" show-if-above class="q-py-md" :style="{ width: '200px' }">
-        <q-separator class="q-my-xl" color="gray-blue-10" inset />
-        <div class="column justify-center items-center sidebar-login-btn-container">
-          <q-btn class="register-btn btn-fixed-width"><a>ثبت نام</a></q-btn>
-          <q-btn class="entrance-btn btn-fixed-width"><a>ورود</a></q-btn>
-        </div>
-        <q-list class="q-mt-md">
-          <q-item v-for="(item, index) in sortedMenuItems.reverse()"  :key="index"   class="navLink-item">
-            <submenu class="navLink-btn" :isSubMenu="hasChildItems(item.id)" :menus="getChildItems(item.id)" :menu="item" :allMenu = "InsuranceNavbarMenuItems" isFirst/>
-          </q-item>
-        </q-list>
-      </q-drawer>
-    </div>
+
+       <div class="logo-container q-ml-auto">
+        <q-img src="../../assets/logo.png" width="200px"></q-img>
+      </div>
+      </div>
+ 
   </q-toolbar>
 </template>
 
 
 <script>
-import { defineComponent, ref } from "vue";
-import apiService from "src/services/api-services";
-import submenu from 'src/components/Header/submenu.vue';
+import { defineComponent} from "vue";
 export default defineComponent({
   name: "ToolbarNavigation",
-  components:{
-    submenu
-  },
-  data() {
-      return {
-        InsuranceHeaderInfo: null,
-        InsuranceNavbarMenuItems: [],
-      };
-    },
-  
-    mounted() {
-      this.getPolicyIntroduction();
-      this.getMenuesItem();
-    },
-  
-    methods: {
-      getPolicyIntroduction() {
-        apiService
-          .getPolicyIntroduction('sabz')
-          .then((response) => {
-            this.InsuranceHeaderInfo = response.data.message?.insuranceCentrePortal;
-            
-          })
-          .catch((error) => {
-            console.error('Error fetching insurance centre info:', error);
-          });
-      },
-      getMenuesItem() {
-        apiService
-          .getMenuesItem('sabz')
-          .then((response) => {
-            this.InsuranceNavbarMenuItems = response.data.message;
 
-          })
-          .catch((error) => {
-            console.error('Error fetching insurance centre info:', error);
-          });
-      },
-      hasChildItems(itemId){
-          return this.InsuranceNavbarMenuItems.filter(item=> item.parentId == itemId).length > 0;
-        
-      } ,
-      getChildItems(itemId){
-        return this.InsuranceNavbarMenuItems.filter((item) => item.parentId == itemId)
-      }
-    },
-    computed: {
-      sortedMenuItems() {
-      return this.InsuranceNavbarMenuItems.filter(item => item.parentId == null).sort((a, b) => b.order - a.order)
-    
-    }
-},
-  setup() {
-    const sidebarVisible = ref(false);
-    function toggleSidebar() {
-      sidebarVisible.value = !sidebarVisible.value;
-    }
 
-    return {
-      sidebarVisible,
-      toggleSidebar,
-    };
-  },
 });
 </script>
 <style scoped>
-.title-container {
-  margin-left : 440px !important;
-  text-align: right !important;
+.search-btn{
+  width: 48px;
+  border : 2px solid #ABABAB;
+  border-radius: 16px;
+  background: #FFFFFF;
 }
-.title {
-  font-size: 17px;
-  white-space:nowrap; 
-  width:100%;
-  margin-left: auto;
-  font-weight: 500;
-  
+.entrance-btn{
+  width: 160px;
+  height: 40px;
+  background: #eee;
+  color : #007BFF;
+  font-size:17px;
+  border : 2px solid #007BFF;
+  border-radius: 16px;
 }
-*{
-  padding: 0 !important;
+.register-btn{
+  width: 160px;
+  height: 40px;
+  background: #007BFF;
+  color : #FFFFFF;
+  font-size:17px;
+  border-radius: 16px;
 }
-.entrance-btn {
-  /* padding: 8px 50px !important; */
-  background: #3c9e75 !important;
-  color: #fff;
-  text-align: center;
-  cursor: pointer;
-  margin-left: 20px !important;
-  font-size: 14px;
-  font-weight: 500;
-  width: 130px;
-}
-.register-btn {
-  width: 100px;
-  margin-right: 10px;
-  background: #a8cf8c !important;
-  color: #fff;
-  text-align: center;
-  cursor: pointer;
-  font-size: 14px;
-  font-weight: 500;
-}
-
-.navLink-container {
-  list-style-type: none;
-}
-
-.navLink-item a {
-  text-decoration: none;
-  color: #fff;
-  cursor: pointer;
-}
-.navLink-btn {
-  font-size: 14px;
-}
-.hambergur-menu {
-  display: none;
-}
-
-.sidebar-navLink-container {
-  display: none !important;
-  list-style-type: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: space-between;
-}
-
-.sidebar-login-btn-container {
-  text-align: center;
-}
-.sidebar-navLink-item {
-  margin: 5px 10px;
-}
-.sidebar-login-btn-container .entrance-btn {
-  max-width: 100%; /* Make the buttons fill the full width */
-  padding: 8px 30px !important;
-  background: #3c9e75 !important;
-  color: #fff;
-  text-align: center;
-  cursor: pointer;
-  margin-top: 5px;
-  font-size: 14px;
-  font-weight: 500;
-}
-.sidebar-login-btn-container .register-btn {
-  /* Make the buttons fill the full width */
-  padding: 8px 50px !important;
-  background: #3c9e75 !important;
-  color: #fff;
-  white-space: nowrap;
-  text-align: center;
-  cursor: pointer;
-  margin-top: 5px;
-  font-size: 14px;
-  font-weight: 500;
-}
-
-.sidebar-navLink-item a {
-  text-decoration: none;
-  color: #fff;
-  cursor: pointer;
-}
-
-.sidebar-navLink-btn {
-  font-size: 14px;
-}
-
-@media only screen and (max-width: 990px) {
-  .navLink-container {
-    display: none;
-  }
-  .title-container {
-    margin-left: 300px !important;
-  text-align: right !important;
-}
-  .login-btn-group {
-    display: none;
-  }
-
-  .title {
-    font-size: 17px;
-  }
-
-  .hambergur-menu {
-    display: block;
-    margin-left: 10px;
-  }
-
-  .sidebar-navLink-container {
-    display: block;
-  }
-  .register-btn , .entrance-btn {
-    min-width:230px;
-    width: 60%;
-    text-align: center;
-    margin: 6px auto !important;
-  }
-}
-@media only screen and (max-width: 727px) {
-
-  .hambergur-menu {
-    display: block;
-    /* margin-left: 65px; */
-  }
-  .title-container{
-    margin-left: 200px !important;
-  }
-
+.toolbar-content{
+  width: 100%;
 }
 </style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,43 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <div class="style relative-position" >
+      <q-header  class="q-pa-md   absolute text-white" elevated style="width: 1280px; margin: auto; border-radius: 1000px;margin-top:60px;background: #CCE4FF;">
+      <ToolbarNavigation/>
+    </q-header>
+    <h1 class="absolute fixed-center  banner-title text-white flex justify-center items-center">بیمه،راه جبران خسارت</h1>
+    </div>
     <q-page-container>
       <router-view />
     </q-page-container>
+    <FooterSection/>
   </q-layout>
 </template>
 
 <script>
 import { defineComponent, } from 'vue'
-
+import ToolbarNavigation from '../components/Header/toolbar-navigation.vue';
+import FooterSection from '../components/Footer-Layout/footer-section.vue';
 
 
 export default defineComponent({
   name: 'MainLayout',
-
+ 
+  components: {
+    ToolbarNavigation,
+    FooterSection
+  },
 })
 </script>
+<style scoped>
+.style{
+  background: url(../assets/banner.png);
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 600px;
+  opacity:0/80;
+}
+.banner-title{
+  font-weight: 900;
+}
+</style>
