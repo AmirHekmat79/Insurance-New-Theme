c4f8c016cdb0ac8000a237cb4c582b2261f25f38
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-07 16:51:24 +0330

    Add Type Cards

---
+++ src/components/Insurance-Type/insurance-type-cards.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        asdada
+      </q-card-section>
+    </q-card>
+</template>
