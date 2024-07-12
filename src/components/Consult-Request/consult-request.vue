a8eb68e50976bbf41de0258a5fb446b91fab74fb
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-12 18:28:37 +0330

    Change Inputs Alignment

--- src/components/Consult-Request/consult-request.vue
+++ src/components/Consult-Request/consult-request.vue
@@ -1,83 +1,91 @@
 <template>
-    <div  class="row q-my-xl justify-center text-center reverse-wrap  items-center q-py-lg q-gutter-md">
-        <div class="col-md-6 col-sm-6 col-xs-12">
-         <form>
-                <q-input   class="Input"  rounded  standout="bg-blue-3 text-white" v-model="text"  >
-                    <template v-slot:prepend>
-                       <img class="img-inner" src="src/assets/person.svg">
-                    </template>
-                    <template v-slot:append>
-                       <label  class="text-subtitle2 label-font">نام خانوادگی  </label>
-                    </template>             
-                </q-input>
-                <q-input  class="q-my-md Input" rounded standout="bg-blue-3 text-white" v-model="text"  >
-                    <template v-slot:prepend>
-                        <img  class="img-inner" src="src/assets/phone.svg">
-                     </template>
-                    <template v-slot:append>
-                       <label  class="text-subtitle2 label-font"> شماره همراه  </label>
-                    </template> 
-                </q-input>
-                <q-input class="Input"  rounded  standout="bg-blue-3 text-white" v-model="text" >
-                    <template v-slot:prepend>
-                       <img class="img-inner" src="src/assets/umberella.svg">
-                    </template>
-                    <template v-slot:append>
-                       <label  class="text-subtitle2 label-font"> نوع بیمه  </label>
-                    </template> 
-                </q-input>
-                <q-btn class="submit-request q-my-md"> <q-img class="q-mx-sm" src="../../assets/requestSubmit.png" width="20px"></q-img>ثبت درخواست</q-btn>
-         </form>
-        </div>
-      <div class="col-md-4 col-sm-6 col-xs-12">
-           <h5 class=" consult-title">درخواست مشاوره</h5>
-           <q-img class="consult-img" src="../../assets/consultRequest.png"></q-img>
-      </div>
+  <div
+    class="row q-my-xl justify-center text-center reverse-wrap items-center q-py-lg q-gutter-md"
+  >
+    <div class="col-md-6 col-sm-6 col-xs-12">
+      <form>
+        <q-input class="Input" rounded outlined v-model="text">
+          <template v-slot:append>
+            <img class="img-inner" src="src/assets/person.svg" />
+          </template>
+          <template v-slot:prepend>
+            <label class="text-subtitle2 label-font q-ml-sm"
+              >نام خانوادگی
+            </label>
+          </template>
+        </q-input>
+        <q-input class="q-my-md Input" outlined rounded v-model="text">
+          <template v-slot:append>
+            <img class="img-inner" src="src/assets/phone.svg" />
+          </template>
+          <template v-slot:prepend>
+            <label class="text-subtitle2 label-font q-ml-sm">
+              شماره همراه
+            </label>
+          </template>
+        </q-input>
+        <q-input class="Input" rounded outlined v-model="text">
+          <template v-slot:append>
+            <img class="img-inner" src="src/assets/umberella.svg" />
+          </template>
+          <template v-slot:prepend>
+            <label class="text-subtitle2 label-font q-ml-sm"> نوع بیمه </label>
+          </template>
+        </q-input>
+        <q-btn class="submit-request q-my-md">
+          <q-img
+            class="q-mx-sm"
+            src="../../assets/requestSubmit.png"
+            width="20px"
+          ></q-img
+          >ثبت درخواست</q-btn
+        >
+      </form>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+      <h5 class="consult-title">درخواست مشاوره</h5>
+      <q-img class="consult-img" src="../../assets/consultRequest.png"></q-img>
+    </div>
   </div>
 </template>
-      
-<script>
- import { defineComponent } from 'vue';
 
-   
+<script>
+import { defineComponent } from "vue";
+
 export default defineComponent({
-        name: "ConsultRequest",
- 
-      
- });
+  name: "ConsultRequest",
+});
 </script>
-      
+
 <style scoped>
-.consult-img{
-    width:490px !important;
-     height:372px !important;
-     
+.consult-img {
+  width: 490px !important;
+  height: 372px !important;
 }
-.submit-request{
-    background: #007BFF;
-    color: #fff;
+.submit-request {
+  background: #007bff;
+  color: #fff;
 }
-.consult-title{
-    font-size: 20px;
-    font-weight: bolder;
+.consult-title {
+  font-size: 20px;
+  font-weight: bolder;
 }
 @media screen and (min-width: 381px) and (max-width: 768px) {
-    .Input{
-        width: 100%;
-    }
-    .consult-img{
-        width:100% !important;
-        height:100% !important;
-        
-    }
-} 
-.img-inner{
-    width: 24px;
-    height: 24px;
+  .Input {
+    width: 100%;
+  }
+  .consult-img {
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+.img-inner {
+  width: 24px;
+  height: 24px;
 }
 
-.Input{
-       text-align: right;
-       cursor: end;
+.Input {
+  text-align: right;
+  direction: rtl;
 }
 </style>
--- src/components/Prolong-Insurance/insurance-form.vue
+++ src/components/Prolong-Insurance/insurance-form.vue
@@ -1,141 +1,142 @@
 <template>
-    <div class="row justify-center items-center q-pa-lg q-my-lg">
-        <div class="col-md-8">
-            <div class="row justify-center items-center q-gutter-md">
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/month.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">ماه</label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/email.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font"> ایمیل</label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img  class="img-inner" src="src/assets/phone.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font"> شماره همراه</label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/umberella.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">نوع بیمه</label>
-                                </template>
-                                
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/location.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">استان </label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/person.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">نام خانوادگی  </label>
-                                </template>
-                        </q-input>  
-                           </div>
-                          
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/month.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label class="text-subtitle2 label-font">   روز</label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/person.svg">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">نام  </label>
-                                </template>
-                        </q-input>  
-                           </div>
-                           <div class="col-md-4">
-                            <q-input  class="Input" rounded outlined v-model="text">
-                                <template v-slot:prepend>
-                                    <img class="img-inner" src="src/assets/building.png">
-                                </template>
-                                <template v-slot:append>
-                                    <label  class="text-subtitle2 label-font">شهر </label>
-                                </template>
-                        </q-input>  
-                        
-                           </div>
-                       <div class="col-md-4">
-                        <q-btn class="submit-btn">یادآوری کن</q-btn>
-                       </div>
-            </div>
+  <div class="row justify-center items-center q-pa-lg q-my-lg">
+    <div class="col-md-8">
+      <div class="row justify-center items-center q-gutter-md">
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/month.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">ماه</label>
+            </template>
+          </q-input>
         </div>
         <div class="col-md-4">
-            <h2 class="form-title">یادآور تمدید بیمه نامه</h2>
-            <q-img class="form-img" src="src/assets/calender.png"></q-img>
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/email.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm"> ایمیل</label>
+            </template>
+          </q-input>
         </div>
-    </div>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue';
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/phone.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">
+                شماره همراه</label
+              >
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/umberella.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">نوع بیمه</label>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/location.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">استان </label>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/person.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm"
+                >نام خانوادگی
+              </label>
+            </template>
+          </q-input>
+        </div>
 
-  
-  export default defineComponent({
-    name: 'InsuranceForm',
-  
-   
-  });
-  </script>
-  
-  <style scoped>
- 
-  .form-img{
-    width: 300px;
-    
-  }
-  .form-title{
-    font-size: 26px;
-    color: #003975;
-  }
-  .submit-btn{
-    cursor: pointer;
-    background: #007BFF;
-    color:#fff;
-    padding: 8px 24px;
-  }
-  .img-inner{
-    width: 24px;
-    height: 24px;
-  }
-  </style>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/month.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm"> روز</label>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/person.svg" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">نام </label>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-input class="Input" rounded outlined v-model="text">
+            <template v-slot:append>
+              <img class="img-inner" src="src/assets/building.png" />
+            </template>
+            <template v-slot:prepend>
+              <label class="text-subtitle2 label-font q-ml-sm">شهر </label>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-md-4">
+          <q-btn class="submit-btn">یادآوری کن</q-btn>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h2 class="form-title">یادآور تمدید بیمه نامه</h2>
+      <q-img class="form-img" src="src/assets/calender.png"></q-img>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InsuranceForm",
+});
+</script>
+
+<style scoped>
+.form-img {
+  width: 300px;
+}
+.form-title {
+  font-size: 26px;
+  color: #003975;
+}
+.submit-btn {
+  cursor: pointer;
+  background: #007bff;
+  color: #fff;
+  padding: 8px 24px;
+}
+.img-inner {
+  width: 24px;
+  height: 24px;
+}
+.Input {
+  text-align: right;
+  direction: rtl;
+}
+</style>
