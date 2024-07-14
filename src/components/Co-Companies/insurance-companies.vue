4a7db0f8f57b224bcd5876e0f01ff3f70e35cec3
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-14 08:55:19 +0330

    Change Insurance Cards Styles

--- src/components/Co-Companies/insurance-companies.vue
+++ src/components/Co-Companies/insurance-companies.vue
@@ -7,17 +7,34 @@
         
         </div>
       </div>
-      <div class="container">
-         <div class="row justify-around items-center">
-          <q-card class="card" v-for="item in InsuranceCompanies" :key="item.id">
+      <div class="container q-px-lg q-mx-auto">
+         <div class="row justify-center items-center text-center q-gutter-md">
+          <div class="col-md-2 change-opacity" v-for="item in InsuranceCompanies.filter(item=>item.id!=32).slice(7,9)" :key="item.id">
+            <q-card class="card shadow-10">
             <q-card-section>
-              <q-img :src="item.metaMediaLogoFileUrl" width="125px" :ratio="1/1" fit="contain"> </q-img>
+              <q-img :src="item.metaMediaLogoFileUrl" width="125px"> </q-img>
+            </q-card-section>
+          </q-card>
+          </div>
+          <div class="col-md-2" v-for="item in InsuranceCompanies.filter(item=>item.id==32)" :key="item.id">
+            <q-card class="card shadow-10">
+            <q-card-section>
+              <q-img :src="item.metaMediaLogoFileUrl" width="125px"> </q-img>
             </q-card-section>
           </q-card>
 
+          </div>
+          <div class="col-md-2 change-opacity" v-for="item in InsuranceCompanies.filter(item=>item.id!=32).slice(3,5)" :key="item.id">
+            <q-card class="card shadow-10">
+            <q-card-section>
+              <q-img :src="item.metaMediaLogoFileUrl" width="125px"> </q-img>
+            </q-card-section>
+          </q-card>
+
+          </div>
 
 
-          
+
          </div>
       </div>
     </section>
@@ -76,5 +93,12 @@ export default defineComponent({
   display: inline-block;
 }
 
- 
+.change-opacity{
+  opacity: 0.5;
+} 
+@media screen and (max-width:892px) {
+  .change-opacity{
+    display: none;
+  }
+}
 </style>
