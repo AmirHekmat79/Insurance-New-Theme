52d1c3b0d0ac8fd3e27be55bd9b3fe394d48440f
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-28 14:19:56 +0330

    Add Insurance Magazine Grid And Data

--- src/pages/InsuranceMagazine.vue
+++ src/pages/InsuranceMagazine.vue
@@ -1,23 +1,49 @@
 <template>
     <q-page style="min-height:400px">  
       <section dir="rtl" class="q-py-lg ">
-       <div class="row justify-around items-center text-center q-gutter-sm">
-         <div class="col-md-3" v-for="n in 4" :key="n">
-            <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-                <div class="absolute-bottom text-h6">
-                Title
-                </div>
-            </q-img>
+       <div class="row justify-center items-center text-center q-gutter-sm">
+        <div class="col-md-6">
+            <div class="row justify-center items-center q-gutter-sm">
+                <div class="col-lg-4 card-item" v-for="item in magazines" :key="item.id">
+                <q-card class="my-card">
+                <q-img class="card-image" :src="item.src">
+                    <div class="absolute-bottom text-h6">
+                    {{ item.title }}
+                    </div>
+                </q-img>
 
-            <q-card-section>
-                asdad
-            </q-card-section>
-    </q-card>
+                <q-card-section>
+                {{ item.caption }}
+                </q-card-section>
+                
+            <div>
+                <q-separator light/>
+            </div>
+            <q-btn flat color="primary">
+                ادامه مطلب
+            </q-btn>
+        </q-card>
          </div>
-         <!-- <div class="col-md-5">
-
-         </div> -->
+            </div>
+        </div>
+         <div class="col-lg-4">
+                <q-card class="my-card">
+           
+                <q-card-section v-for="item in magazines" :key="item.id">
+                        <div class="row justify-between items-center">
+                            {{ item.title }}
+                              <div class="text-primary">
+                                مشاهده
+                              </div>
+                        </div>
+                </q-card-section>
+                
+            <div>
+                <q-separator light/>
+            </div>
+          
+        </q-card>
+         </div>
        </div>
       </section>
     </q-page>
@@ -28,13 +54,30 @@
 
   export default defineComponent({
     name: "InsuranceMagazine",
-    components: {
-    
-    },
+    data(){
+        return{
+            magazines : [
+                {id : 1 , title : 'بیمه مسافرتی' , src:'src/assets/national-insurance.jpg'  , caption : 'بیمه مسافرتی، بیمه ایست که از شما در برابر خطراتی که در طول سفر تهدیدتان میکند، محافظت میکند.'} ,
+                {id : 2 , title : 'بیمه عمر' , src:'src/assets/rain.png' , caption : ' بیمه عمر از دیدگاه حقوقی، قراردادی است که به موجب آن بیمه‌گر در مقابل دریافت حق‌بیمه از بیمه‌گذار متعهد می‌شود'} ,
+                {id : 3 , title : 'بیمه بدنه' , src:'src/assets/body-insurance.jpg' , caption : 'بیمه بدنه، بیمه‌های اختیاری است که میتواند به عنوان مکمل بیمه شخص ثالث استفاده شود.    .'} ,
+                {id : 4 , title : 'بیمه آتش سوزی' , src:'src/assets/fire-insurance.jpg' , caption : 'بیمه آتش سوزی ساختمان، بیمه‌های اختیاری است که متأسفانه لزوم خرید آن، زمانی که خیلی دیر شده است، احساس میشود...'} ,
+                {id : 5 , title : 'بیمه شخص ثالث' , src:'src/assets/car-insurance.jpg' , caption : 'بیمه شخص ثالث، طرح بیمه‌های الزامی برای دارندگان وسایل نقلیه موتوری است.'} ,
+
+            ]
+        }
+    }
+   
   });
   </script>
   
 <style scoped>
-
+.card-image{
+    height: 30vh;
+    background-position: center;
+    background-size: cover;
+}
+/* .card-item{
+    height: 500px !important;
+} */
 </style>
   
