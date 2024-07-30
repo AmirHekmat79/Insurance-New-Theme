5ab188248100490f2cb053708cdca65bab8cdd2b
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-30 11:18:02 +0330

    Change InsuranceMagazine Layout

--- src/pages/InsuranceMagazine.vue
+++ src/pages/InsuranceMagazine.vue
@@ -1,75 +1,58 @@
 <template>
-    <q-page style="min-height:400px">  
+    <q-page >  
       <section dir="rtl" class="q-py-lg ">
-       <div class="row justify-center items-center text-center q-gutter-sm  only-in-large">
-        <div style="height: auto !important;" class="col-lg-4">
-                <q-card class="my-card" >
+       <div class="row justify-center items-center text-center q-gutter-sm ">
+        <div style="height: auto !important;width: 300px !important;" class="col-lg-4">
+                <div class="my-card curve" >
                     <h5 class="text-center  q-pt-lg">مطالب جدید</h5>
                     <div class="filter-title-underline1"></div>
                     <div class="filter-title-underline2"></div>
-                <q-card-section v-for="item in magazines" :key="item.id" class="q-my-lg">
-                        <div class="row justify-between items-center">
-                            {{ item.title }}
-                              <q-btn @click="goToDetails(item)" flat class="text-primary cursor-pointer">
-                                مشاهده جزِئیات
+                <div v-for="item in magazines" :key="item.id" class="q-my-lg">
+                        <div class="column justify-center items-center">
+                            <q-card class="sub-blog-card">
+                              <div class="row justify-between items-center q-ml-md">
+                                  <q-img class="card-side-image q-mx-sm" :src="item.src"> </q-img>
+                               <p class="q-mx-sm item-title"> {{ item.title }}</p>
+                              </div>
+                              <div class="row justify-between items-center q-mr-md">
+                                <p class="q-mx-sm item-date text-blue-grey-5">
+                                    ۲۹ تیر | ۱۴:۳۰
+                                </p>
+                                <q-btn @click="goToDetails(item)" flat class="text-primary cursor-pointer">
+                                 ادامه مطلب
                               </q-btn>
+                              </div>
+                             
+                            </q-card>
                             </div>
                             <q-separator light class="q-my-lg" />
-                </q-card-section>
+                </div>
                 
             <div>
-                <q-separator light/>
+                <!-- <q-separator light/> -->
             </div>
           
-        </q-card>
-         </div>
-        <div class="col-md-6" >
-            <div class="row justify-center items-center q-gutter-sm">
-                <div class="col-lg-4 card-item" v-for="item in magazines" :key="item.id">
-                <q-card class="my-card blog-card">
-                <q-img class="card-image" :src="item.src">
-                    <div class="absolute-bottom text-h6">
-                    {{ item.title }}
-                    </div>
-                </q-img>
-
-                <q-card-section>
-                {{ item.caption }}
-                </q-card-section>
-                
-            <div>
-                <q-separator light/>
-            </div>
-            <q-btn flat color="primary" @click="goToDetails(item)">
-                ادامه مطلب
-            </q-btn>
-        </q-card>
-         </div>
-            </div>
         </div>
-       
-       </div>
-       <div class="row justify-center items-center q-gutter-sm text-center only-in-small">
-        <div class="col-sm-6 card-item" v-for="item in magazines" :key="item.id">
-                <q-card class="my-card">
-                <q-img class="card-image" :src="item.src">
-                    <div class="absolute-bottom text-h6">
-                    {{ item.title }}
-                    </div>
-                </q-img>
-
-                <q-card-section>
-                {{ item.caption }}
-                </q-card-section>
-                
-            <div>
-                <q-separator light/>
-            </div>
-            <q-btn flat color="primary" @click="goToDetails(item)">
-                ادامه مطلب
-            </q-btn>
-        </q-card>
          </div>
+  
+    <div class="col-md-3 q-mx-md" v-for="item in magazines.filter(item => item.id == 5)" :key="item.id">
+          <q-img :src="item.src"  class="card-img" style="width: 500px; height: 430px;">
+     <div class="absolute-full text-subtitle2 flex flex-center">
+       {{ item.title }}
+     </div>
+   </q-img>
+    </div>
+    <div class="col-md-6" >
+            <div class="row justify-center items-center q-gutter-sm">
+                <div class="col-lg-4 card-item" v-for="item in magazines.filter(item => item.id !== 5)" :key="item.id">
+        <q-img :src="item.src" class="card-img">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            {{ item.title }}
+          </div>
+        </q-img>
+         </div>
+        </div>
+    </div>
        </div>
       </section>
     </q-page>
@@ -87,7 +70,7 @@
                 {id : 2 , title : 'بیمه عمر' , src:'src/assets/rain.png' , caption : ' بیمه عمر از دیدگاه حقوقی، قراردادی است که به موجب آن بیمه‌گر در مقابل دریافت حق‌بیمه از بیمه‌گذار متعهد می‌شود'} ,
                 {id : 3 , title : 'بیمه بدنه' , src:'src/assets/body-insurance.jpg' , caption : 'همچنین مزیت های دیگری نیز دارد.بیمه بدنه، بیمه‌های اختیاری است که میتواند به عنوان مکمل بیمه شخص ثالث استفاده شود.    .'} ,
                 {id : 4 , title : 'بیمه آتش سوزی' , src:'src/assets/fire-insurance.jpg' , caption : 'بیمه آتش سوزی ساختمان، بیمه‌های اختیاری است که متأسفانه لزوم خرید آن، زمانی که خیلی دیر شده است، احساس میشود...'} ,
-                // {id : 5 , title : 'بیمه شخص ثالث' , src:'src/assets/car-insurance.jpg' , caption : 'بیمه شخص ثالث، طرح بیمه‌های الزامی برای دارندگان وسایل نقلیه موتوری است.'} ,
+                {id : 5 , title : 'بیمه شخص ثالث' , src:'src/assets/car-insurance.jpg' , caption : 'بیمه شخص ثالث، طرح بیمه‌های الزامی برای دارندگان وسایل نقلیه موتوری است.'} ,
 
             ]
         }
@@ -107,10 +90,36 @@
   </script>
   
 <style scoped>
+.curve{
+    border : 1px solid #eee;
+    border-radius: 5px !important;
+}
+.item-title{
+    font-size: 14px;
+}
+.card-img{
+    width: 100%;
+    height: 300px;
+    display: block;
+    object-fit: cover;
+    border-radius: 5px;
+}
+.item-date{
+    font-size: 100%;
+}
+.sub-blog-card{
+    width:260px;
+    padding: 8px;
+}
 .card-image{
     height: 30vh;
     background-position: center;
     background-size: cover;
+}
+.card-side-image{
+    width: 60px;
+    height: 60px;
+    border-radius: 5px;
 }
 /* .card-item{
     height: 500px !important;
@@ -134,7 +143,7 @@ h5{
     background: #2980b9;
 }
 
-@media screen and (max-width : 1030px) {
+/* @media screen and (max-width : 1030px) {
     .only-in-large{
         display: none;
     }
@@ -149,6 +158,6 @@ h5{
    .blog-card{
     width: 300px;
    }
-}
+} */
 </style>
   
