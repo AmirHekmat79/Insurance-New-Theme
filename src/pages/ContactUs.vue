0116022a75446124ff0a4750c3e4c3281f0bdc29
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-08-03 15:26:36 +0330

    Change Contact Us Layout

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,29 +1,14 @@
 <template>
-    <q-page style="min-height:400px">
-    <div class="row justify-around items-center">
-        <div class="shape6-img3 text-right q-ma-lg">
-    <q-img  src="../assets/shape1.svg" width="15px"></q-img>
-   </div>
-        <div class="text-right q-ma-lg">
-            <q-img class="shape2-img3" src="../assets/triangle.svg" width="20px"></q-img>
-        </div>
-   
-    <div class="shape4-img3 text-right q-ma-lg">
-        <q-img  src="../assets/shape5.png" width="25px"></q-img>
-    </div>
-    <div class="shape3-img3 text-right q-ma-lg">
-        <q-img  src="../assets/6.png" width="40px"></q-img>
-    </div>
-    </div>
-        <div class="title text-right q-py-md q-mr-sm">
-            <span class="q-pr-sm text-primary">تماس با ما</span>
-            <q-img src="../assets/phone.svg" width="22px"></q-img>
+    <q-page>
+        <div class="title text-center q-py-md q-mr-sm">
+            <span class="q-pr-sm text-primary text-weight-bolder contact-title">تماس با ما</span>
+            <q-img src="../assets/phone.svg" width="20px"></q-img>
           </div>
       <section dir="rtl" class="q-py-lg q-mx-auto text-center">
 
-          <div class="row justify-center items-center q-gutter-lg">
-            <div class="col-md-4 col-sm-6 col-xs-12 q-gutter-md">
-              <q-input v-model="text"  filled
+          <div class="row justify-center items-center  q-gutter-lg">
+            <div class="col-md-4 col-sm-6 col-xs-12 text-center q-gutter-md form-bg q-pa-lg rounded-borders ">
+              <q-input v-model="text"  bg-color="white" filled
                 >
                 <template v-slot:append>
                     <q-avatar>
@@ -34,7 +19,7 @@
                             <label>نام و نام خانوادگی</label>
                     </template>
                 </q-input>
-              <q-input v-model="text"  filled
+              <q-input v-model="text"  bg-color="white" filled
                 >
                 <template v-slot:append>
                     <q-avatar>
@@ -45,7 +30,7 @@
                             <label>تلفن همراه  </label>
                     </template>
                 </q-input>
-              <q-input v-model="text"  filled
+              <q-input v-model="text" bg-color="white"  filled
                 >
                 <template v-slot:append>
                     <q-avatar>
@@ -62,66 +47,69 @@
                 v-model="message"
                 type="textarea"
                 placeholder="متن پیام شما..."
+                bg-color="white"
                 filled
                 rows="4"
             />
-            <q-btn class="bg-primary text-white">ارسال</q-btn>
+            <q-btn style="width: 50%;" class="bg-primary text-center q-mx-auto text-white">ارسال</q-btn>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 custom-bg rounded-borders font-style">
+             <div class="info-container">
               <div class=" q-my-md ">
-                <span  class=" text-primary">
+                <span  class="text-warning">
                     شماره تلفن :
                 </span>
-                <span>
+                <span class="text-white">
                     02188308288</span>
               </div>
               <div class=" q-my-md ">
-                <span class="text-primary">
+                <span class="text-warning">
                      آدرس :
                 </span>
-                <span>
+                <span class="text-white">
                     تهران خیابان قائم مقام فراهانی، خیابان سنایی، خیابان مشاهیر، خیابان غفاری، پلاک 12</span>
               </div>
               <div class=" q-my-md ">
-                <span  class=" q-my-md text-primary">
+                <span   class="q-my-md text-warning">
                     ایمیل :
                 </span>
-                <span>
+                <span class="text-white">
                     
             info@qbimeh.ir
         </span>
               </div>
               <div>
-                  <span  class="text-primary text-center">
+                  <span  class="text-warning text-center">
                       شبکه های اجتماعی : 
                   
                   </span>
                   <a class="cursor-pointer">
-          <q-img width="20px" src="/src/icons/whatsapp.svg"/>
+          <q-img width="22px" src="/src/icons/whatsapp.svg"/>
          </a>
       <a class="cursor-pointer">
-          <q-img width="20px" src="/src/icons/twitter-x.svg"/>
+          <q-img width="22px" src="/src/icons/twitter-x.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img  width="20px"  src="/src/icons/telegram.svg"/>
+                  <q-img  width="22px"  src="/src/icons/telegram.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img   width="20px" src="/src/icons/robika.svg"/>
+                  <q-img   width="22px" src="/src/icons/robika.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img  width="20px"  src="/src/icons/linkedin.svg"/>
+                  <q-img  width="22px"  src="/src/icons/linkedin.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img   width="20px" src="/src/icons/instagram.svg"/>
+                  <q-img   width="22px" src="/src/icons/instagram.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img   width="20px" src="/src/icons/facebook.svg"/>
+                  <q-img   width="22px" src="/src/icons/facebook.svg"/>
               </a>
               <a class="cursor-pointer">
-                  <q-img   width="20px"  src="/src/icons/eitaa.svg"/>
+                  <q-img   width="22px"  src="/src/icons/eitaa.svg"/>
               </a>
-              <a class="cursor-pointer"><q-img width="20px" src="/src/icons/bale.svg"/></a>  
+              <a class="cursor-pointer"><q-img width="22px" src="/src/icons/bale.svg"/></a>  
                 </div>
+             </div>
             </div>
           </div>
       </section>
@@ -145,6 +133,35 @@
   </script>
   
 <style scoped>
+.form-bg{
+  background: #EEEE;
+  background-position: center;
+  background-size: cover;
+  height: 80vh;
+  
+
+}
+.contact-title{
+  font-size: 17px;
+}
+.font-style{
+  font-size: 18px;
+}
+.info-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
+}
+.custom-bg{
+  background-image: url(../assets/insurance-companies.jpg);
+  background-position: center;
+  background-size: cover;
+  height: 80vh;
+  filter:grayscale(0.5);
+
+}
  .cursor-pointer{
     margin: 10px;
  }
